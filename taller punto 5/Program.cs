44c94418d89abda6d719c158c3251ab82473811f
Jorge <Jorge@DESKTOP-MSOVDS0>
2023-09-01 19:35:49 -0500

    solucion taller punto 5

---
+++ taller punto 5/Program.cs
@@ -0,0 +1,21 @@
+﻿// Dados los catetos de un triángulo rectángulo, calcular su hipotenusa.
+
+public class TallerPunto5
+{
+    static void Main(string[] args)
+    {
+       double cateto1, cateto2, hipotenusa;
+
+        Console.Write("Ingresar cateto 1:");
+        cateto1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingresar cateto 2:");
+        cateto2 = double.Parse(Console.ReadLine());
+
+        hipotenusa = Math.Sqrt (Math.Pow (cateto1,2) + Math.Pow (cateto2,2));
+
+        Console.Write("la hipotenusa del triangulo es:" + hipotenusa);
+    }
+
+
+}
